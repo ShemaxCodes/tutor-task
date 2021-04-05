e006bd2cdf8e2644e517179b95a4142433f57f6e
Jashema Panapa <jcpanapa@gmail.com>
2021-04-05 11:39:55 -0400

    new file:   app/views/users/confirmations/new.html.erb 	new file:   app/views/users/mailer/confirmation_instructions.html.erb 	new file:   app/views/users/mailer/email_changed.html.erb 	new file:   app/views/users/mailer/password_change.html.erb 	new file:   app/views/users/mailer/reset_password_instructions.html.erb 	new file:   app/views/users/mailer/unlock_instructions.html.erb 	new file:   app/views/users/passwords/edit.html.erb 	new file:   app/views/users/passwords/new.html.erb 	new file:   app/views/users/registrations/edit.html.erb 	new file:   app/views/users/registrations/new.html.erb 	new file:   app/views/users/sessions/new.html.erb 	new file:   app/views/users/shared/_error_messages.html.erb 	new file:   app/views/users/shared/_links.html.erb 	new file:   app/views/users/unlocks/new.html.erb 	modified:   config/routes.rb 	new file:   db/migrate/20210405152526_add_firstname_lastname_to_users.rb 	modified:   db/schema.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "welcome#index"
+
+  get "/users/signup", to: "devise/registrations#new"
+
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_05_151940) do
+ActiveRecord::Schema.define(version: 2021_04_05_152526) do
 
   create_table "assignments", force: :cascade do |t|
     t.string "title"
@@ -39,6 +39,8 @@ ActiveRecord::Schema.define(version: 2021_04_05_151940) do
     t.datetime "updated_at", precision: 6, null: false
     t.integer "role", default: 1
     t.string "username"
+    t.string "first_name"
+    t.string "last_name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
